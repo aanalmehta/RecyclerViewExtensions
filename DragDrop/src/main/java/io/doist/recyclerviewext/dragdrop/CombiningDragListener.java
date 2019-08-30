011135d0b50d0575a3eb45f20b78cc4d97db956f
Gonçalo Silva <goncalossilva@gmail.com>
2019-08-30 03:34:45 +0100

    Add OnDragListener that combiners other OnDragListeners

---
+++ DragDrop/src/main/java/io/doist/recyclerviewext/dragdrop/CombiningDragListener.java
@@ -0,0 +1,32 @@
+package io.doist.recyclerviewext.dragdrop;
+
+import android.view.DragEvent;
+import android.view.View;
+
+/**
+ * A {@link android.view.View.OnDragListener} that combines multiple {@link android.view.View.OnDragListener}.
+ *
+ * The semantics of the result of {@link #onDrag(View, DragEvent)} are retained, meaning listeners that return false
+ * for {@link DragEvent#ACTION_DRAG_STARTED} won't receive further events.
+ */
+public class CombiningDragListener implements View.OnDragListener {
+    private View.OnDragListener[] onDragListeners;
+    private boolean[] dragStartedResults;
+
+    public CombiningDragListener(View.OnDragListener... onDragListeners) {
+        this.onDragListeners = onDragListeners;
+        dragStartedResults = new boolean[onDragListeners.length];
+    }
+
+    @Override
+    public boolean onDrag(View view, DragEvent event) {
+        for (int i = 0; i < onDragListeners.length; i++) {
+            if (event.getAction() == DragEvent.ACTION_DRAG_STARTED) {
+                dragStartedResults[i] = onDragListeners[i].onDrag(view, event);
+            } else if (dragStartedResults[i]) {
+                onDragListeners[i].onDrag(view, event);
+            }
+        }
+        return true;
+    }
+}
